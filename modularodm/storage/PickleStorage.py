9fd0c3e58419d5315011c68486975db1dc114936
Joshua Carp <jm.carp@gmail.com>
2013-07-20 20:40:02 -0400

    added query methods

--- modularodm/storage/PickleStorage.py
+++ modularodm/storage/PickleStorage.py
@@ -1,6 +1,7 @@
 from ..storage import Storage
 from ..storage import KeyExistsException
 
+import operator
 import copy
 import os
 try:
@@ -74,6 +75,43 @@ class PickleStorage(Storage):
 
     def find_all(self):
         return self.store.values()
-    
+
+    def find_one(self, **kwargs):
+
+        results = list(self.find(**kwargs))
+        if len(results) == 1:
+            return results[0]
+
+        raise Exception(
+            'Query for find_one must return exactly one result; returned {0}'.format(
+                len(results)
+            )
+        )
+
+    def find(self, **kwargs):
+
+        for key, value in self.store.iteritems():
+            match = True
+            for field, arg in kwargs.iteritems():
+                field_parse = field.split('__')
+                if len(field_parse) == 1:
+                    # Exact match
+                    match = value[field] == arg
+                elif len(field_parse) == 2:
+                    field_name, field_op = field_parse
+                    if hasattr(value[field_name], field_op):
+                        # Matcher is property of value
+                        match = getattr(value[field_name], field_op)(arg)
+                    elif hasattr(operator, field_op):
+                        # Matcher is provided by operator
+                        match = getattr(operator, field_op)(value[field_name], arg)
+                if not match:
+                    break
+            if match:
+                yield value
+
+    def get(self, key):
+        return self.store[key]
+
     def __repr__(self):
         return str(self.store)
