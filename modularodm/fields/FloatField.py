41d2f878676c4f62938b862ae150d9db507c6b1a
Joshua Carp <jm.carp@gmail.com>
2013-08-06 15:36:18 -0400

    initial commit

---
+++ modularodm/fields/FloatField.py
@@ -0,0 +1,10 @@
+from ..fields import Field
+from ..validators import validate_float
+
+class FloatField(Field):
+
+    default = 0.0
+    validate = validate_float
+
+    def __init__(self, *args, **kwargs):
+        super(FloatField, self).__init__(*args, **kwargs)
