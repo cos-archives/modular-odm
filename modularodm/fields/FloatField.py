279779246b73b2d6e5e7d339a548f5941ae1d73f
Joshua Carp <jm.carp@gmail.com>
2013-08-07 10:10:31 -0400

    no default

--- modularodm/fields/FloatField.py
+++ modularodm/fields/FloatField.py
@@ -3,7 +3,7 @@ from ..validators import validate_float
 
 class FloatField(Field):
 
-    default = 0.0
+    # default = 0.0
     validate = validate_float
 
     def __init__(self, *args, **kwargs):
