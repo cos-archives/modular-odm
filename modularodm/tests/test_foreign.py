033f4661a5344d26462521246960044c8fd152d3
Joshua Carp <jm.carp@gmail.com>
2013-08-07 14:53:10 -0400

    stubbed tests

---
+++ modularodm/tests/test_foreign.py
@@ -0,0 +1,16 @@
+
+import unittest
+
+class TestForeign(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    # One-to-many tests
+
+
+    # Many-to-many tests
+
