2f033c26ca9574a42906d086cc75fdea88bb8350
Jeffrey Spies <jspies@gmail.com>
2013-08-12 15:13:11 -0400

    Caching at a good point; still needs work though.

--- examples/caching_example.py
+++ examples/caching_example.py
@@ -22,7 +22,7 @@ except:pass
 
 class Cache(StoredObject):
     _id = StringField()
-    content = DictionaryField()
+    content = DictionaryField(list=True)
 
 Cache.set_storage(PickleStorage('cache'))
 
@@ -31,37 +31,52 @@ class CachedObject(StoredObject):
     def __init__(self, **kwargs):
         super(CachedObject, self).__init__(**kwargs)
         # tells are foreign keys to pay attention, even if no backrefs?
-        self.cache_id='{}:{}'.format(self._name, self._primary_key)
+        self._add_field('_cache_id', StringField())
+        self._cache_id = '{}:{}'.format(self._name, self._primary_key)
+        self._cache = Cache(_id=self._cache_id)
 
     @classmethod
-    def deep_load_and_collect_keys(cls, key):
+    def deep_load_and_collect_keys(self, key, cls=None):
+        if not cls:
+            cls = self
+        # import pdb; pdb.set_trace()
         obj = cls.load(key) # use cache storage translator
-        keys = [(obj.__class__, obj._primary_key, obj.to_storage())]
+        keys = [{obj._name: obj.to_storage()}]
         for field_descriptor_name, field_descriptor_obj in obj._fields.iteritems():
             if field_descriptor_obj.__class__.__name__ == 'ForeignField':
                 fkey = getattr(obj, field_descriptor_name)._primary_key
-                keys.extend(deep_load_and_collect_keys(field_descriptor_obj._base_class, fkey))
+                keys.extend(self.deep_load_and_collect_keys(fkey, cls=field_descriptor_obj._base_class))
             # handle lists of foreigns
         return keys
 
     @classmethod
-    def load(self, key):
+    def load(cls, key):
+        obj = cls._load_from_cache(key)
+        if obj:
+            return obj
+
         try:
-            value_from_cache = Cache.load(self.cache_id)
-        except:
+            value_from_cache = Cache.load('{}:{}'.format(cls._name, key))
+            for d in value_from_cache.content:
+                k = d.keys()[0]
+                v = d.values()[0]
+                obj = StoredObject._collections[k].from_storage(v)
+                if obj._primary_key == key and k == cls._name:
+                    result = obj
+
+        except Exception as e:
             pass
-        super(CachedObject, self).load(key)
+
+        return result
 
     def save(self):
-        try:
-            data = self.deep_load_and_collect_keys(self.cache_id) # {blog_xyz: {}, tag_python: {}}
-        except:
-            pass
         # Cache(
         #     _id=self.cache_id,
         #     content = data
         # ).save()
         super(CachedObject, self).save()
+        self._cache.content = self.deep_load_and_collect_keys(self._primary_key) # {blog_xyz: {}, tag_python: {}}
+        self._cache.save()
 
 class Keyword(StoredObject):
     _id = StringField()
@@ -80,12 +95,31 @@ Keyword.set_storage(PickleStorage('keyword'))
 Tag.set_storage(PickleStorage('tag'))
 Blog.set_storage(PickleStorage('blog'))
 
-keyword1 = Keyword(_id='test')
+keyword1 = Keyword(_id='keyword1')
 keyword1.save()
 tag1 = Tag(_id='tag1', keyword=keyword1)
 tag1.save()
 blog1 = Blog(_id='blog1', title="hello, world", tag=tag1)
 blog1.save()
-print blog1
 
-print Blog.deep_load_and_collect_keys('blog1')
+import time
+tick = time.time()
+print Blog.load('blog1').tag.keyword
+print time.time() - tick
+
+StoredObject._cache = {}
+StoredObject._object_cache = {}
+
+tick = time.time()
+print Blog.load('blog1').tag.keyword
+print time.time() - tick
+
+# {'_id': 'keyword1'}
+# 4.57763671875e-05
+# {'_id': 'keyword1'}
+# 0.000613927841187
+
+# {'_id': 'keyword1'}
+# 4.6968460083e-05
+# {'_id': 'keyword1'}
+# 0.000807046890259
--- modularodm/fields/DictionaryField.py
+++ modularodm/fields/DictionaryField.py
@@ -1,16 +1,9 @@
 from ..fields import Field
 
-class SchemaDictList(list):
-    def append(self, x):
-        super(SchemaDictList, self).append(x)
-
 class DictionaryField(Field):
+    translate_type = dict
+
     def __init__(self, *args, **kwargs):
         super(self.__class__, self).__init__(*args, **kwargs)
         self._default = kwargs.get('default', {})
-
-    def to_storage(self, value):
-        if value:
-            return [self._base_object.to_storage(i) for i in value]
-        return []
 
