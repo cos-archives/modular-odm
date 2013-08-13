70a7ea3e2ca58710c0e3f450bf8c4e036e65581e
Joshua Carp <jm.carp@gmail.com>
2013-08-13 10:22:10 -0400

    update backref structure

--- modularodm/fields/ForeignField.py
+++ modularodm/fields/ForeignField.py
@@ -2,6 +2,7 @@ from . import Field, List
 from ..StoredObject import StoredObject
 
 import logging
+import warnings
 
 class ForeignList(List):
 
@@ -65,7 +66,7 @@ class ForeignField(Field):
         self._base_class_name = args[0] # todo allow class references / callable?
         self._base_class = None
 
-    def on_after_save(self, parent, old_stored_data, new_value):
+    def on_after_save(self, parent, field_name, old_stored_data, new_value):
         '''
             None, Obj = go from nothing or add
             Obj, None = means go to nothing or remove
@@ -77,13 +78,15 @@ class ForeignField(Field):
 
         if old_stored_data is not None:
             old_value = self.base_class.load(old_stored_data)
-            old_value._remove_backref(self._backref_field_name, parent.__class__, parent._primary_key)
+            old_value._remove_backref(self._backref_field_name, parent.__class__, field_name, parent._primary_key)
 
         if new_value is not None:
-            new_value._set_backref(self._backref_field_name, parent)
+            new_value._set_backref(self._backref_field_name, field_name, parent)
 
     def to_storage(self, value, translator=None):
 
+        if value is None:
+            return value
         if '_primary_key' in dir(value):
             value_to_store = value._primary_key
         else:
@@ -94,6 +97,8 @@ class ForeignField(Field):
 
     def from_storage(self, value, translator=None):
 
+        if value is None:
+            return None
         _foreign_pn = self.base_class._primary_name
         _foreign_pk = self.base_class._fields[_foreign_pn].from_storage(value, translator)
         return self.base_class.load(_foreign_pk)
@@ -118,10 +123,14 @@ class ForeignField(Field):
         return self._base_class
 
     def __set__(self, instance, value):
+        if instance._detached:
+            warnings.warn('Accessing a detached record.')
         StoredObject._must_be_loaded(value)
         super(ForeignField, self).__set__(instance, self.to_primary_key(value))
 
     def __get__(self, instance, owner):
+        if instance._detached:
+            warnings.warn('Accessing a detached record.')
         primary_key = super(ForeignField, self).__get__(instance, None)
         if primary_key is None:
             return
