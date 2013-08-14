8e3cadca3ec6d79e4f4972c004d1e2b14f09d7aa
Lyndsy Simon <lyndsy@lyndsysimon.com>
2013-08-14 12:53:07 -0400

    Tests deleting an object from a ForeignField

--- modularodm/tests/backrefs/test_one_to_many.py
+++ modularodm/tests/backrefs/test_one_to_many.py
@@ -14,20 +14,29 @@ class OneToManyFieldTestCase(PickleStorageTestCase):
         class Bar(TestObject):
             _id = IntegerField()
 
+        # Assign classes ot the test fixture
+        self.Foo = Foo
+        self.Bar = Bar
+
+        # create a Foo and two Bars
         self.foo = Foo(_id=1)
         self.bar = Bar(_id=2)
         self.baz = Bar(_id=3)
 
+        # save the bars so they're in the storage
         self.bar.save()
         self.baz.save()
 
+        # add the bars to the foo
         self.foo.my_bar.append(self.bar)
         self.foo.my_bar.append(self.baz)
+
+        # save foo to persist changes
         self.foo.save()
 
         super(OneToManyFieldTestCase, self).setUp()
 
-    def test_simple_backref(self):
+    def test_one_to_many_backref(self):
 
         # Should be a list of the object's ID
         # TODO: Shouldn't this be the objects themselves? Why not?
@@ -61,4 +70,27 @@ class OneToManyFieldTestCase(PickleStorageTestCase):
         self.assertEqual(
             self.baz._backrefs,
             {'my_foo': {'foo': {'my_bar': [self.foo._id]}}}
+        )
+
+    def test_delete_backref(self):
+        """ Remove an element from a ForeignField, and verify that it was
+        removed from the backref as well.
+        """
+
+        first_bar = self.foo.my_bar[0]
+        second_bar = self.foo.my_bar[1]
+
+        del self.foo.my_bar[0]
+        self.foo.save()
+
+        # The first Bar should be gone from the ForeignField
+        self.assertEqual(
+            list(self.foo.my_bar),
+            [second_bar, ],
+        )
+
+        # The first Bar should no longer have a reference to foo
+        self.assertEqual(
+            first_bar.my_foo,
+            {'foo': {'my_bar': []}}
         )
