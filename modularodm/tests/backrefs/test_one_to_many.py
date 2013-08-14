a950949f8c2e6da4c0eb27781d88ebdb63861001
Lyndsy Simon <lyndsy@lyndsysimon.com>
2013-08-14 14:07:36 -0400

    Adds tests for assignment to and deletion from 1:n ForeignFields

--- modularodm/tests/backrefs/test_one_to_many.py
+++ modularodm/tests/backrefs/test_one_to_many.py
@@ -4,41 +4,92 @@ from modularodm.fields.IntegerField import IntegerField
 from modularodm.fields.ForeignField import ForeignField
 
 
-class Foo(TestObject):
-    _id = IntegerField()
-    my_bar = ForeignField('Bar', backref='my_foo')
+class OneToManyFieldTestCase(PickleStorageTestCase):
 
+    def setUp(self):
 
-class Bar(TestObject):
-    _id = IntegerField()
+        class Foo(TestObject):
+            _id = IntegerField()
+            my_bar = ForeignField('Bar', backref='my_foo')
 
+        class Bar(TestObject):
+            _id = IntegerField()
 
-class OneToOneFieldTestCase(PickleStorageTestCase):
+        self.foo = Foo(_id=1)
+        self.bar = Bar(_id=2)
 
-    def test_simple_backref(self):
-        foo = Foo(_id=1)
-        bar = Bar(_id=2)
+        self.bar.save()
 
-        bar.save()
+        self.foo.my_bar = self.bar
+        self.foo.save()
 
-        foo.my_bar = bar
-        foo.save()
+        super(OneToManyFieldTestCase, self).setUp()
+
+    def test_one_to_one_backref(self):
 
         # The object itself should be assigned
         self.assertIs(
-            foo.my_bar,
-            bar
+            self.foo.my_bar,
+            self.bar
         )
 
         # The backreference on bar should be a dict with the necessary info
         self.assertEqual(
-            bar.my_foo,
-            {'foo': {'my_bar': [foo._id]}}
+            self.bar.my_foo,
+            {'foo': {'my_bar': [self.foo._id]}}
         )
 
         # bar._backrefs should contain a dict with all backref information for
         # the object.
         self.assertEqual(
-            bar._backrefs,
-            {'my_foo': {'foo': {'my_bar': [foo._id]}}}
+            self.bar._backrefs,
+            {'my_foo': {'foo': {'my_bar': [self.foo._id]}}}
         )
+
+    def test_delete_foreign_field(self):
+        """ Remove an element from a ForeignField, and verify that it was
+        removed from the backref as well.
+        """
+
+        del self.foo.my_bar
+        self.foo.save()
+
+        # The first Bar should be gone from the ForeignField
+        self.assertEqual(
+            self.foo.my_bar,
+            None,
+        )
+
+        # The first Bar should no longer have a reference to foo
+        self.assertEqual(
+            self.bar.my_foo,
+            {'foo': {'my_bar': []}}
+        )
+
+    def test_assign_foreign_field_to_none(self):
+        """ Assigning a ForeignField to None should be have the same effect as
+        deleting it.
+        """
+
+        #
+        self.foo.my_bar = None
+        self.foo.save()
+
+        # The first Bar should be gone from the ForeignField
+        self.assertEqual(
+            self.foo.my_bar,
+            None,
+        )
+
+        # The first Bar should no longer have a reference to foo
+        self.assertEqual(
+            self.bar.my_foo,
+            {'foo': {'my_bar': []}}
+        )
+
+    def test_assign_foreign_field_by_id(self):
+        """ Try assigning a ForeignField to the primary key of a remote object
+        """
+
+        # this functionality is not yet defined.
+        self.foo.my_bar = self.bar._id
