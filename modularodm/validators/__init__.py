196f0dded0870d3169fde06df77b5f4eed08d0be
Joshua Carp <jm.carp@gmail.com>
2013-08-15 15:15:15 -0400

    regexvalidator shouldn't inherit from typevalidator; delete old code

--- modularodm/validators/__init__.py
+++ modularodm/validators/__init__.py
@@ -35,48 +35,9 @@ validate_list = TypeValidator(List)
 import datetime
 validate_datetime = TypeValidator(datetime.datetime)
 
-# # class MinLengthValidator(StringValidator):
-# class MinLengthValidator(object):
-#
-#     def __init__(self, min_length):
-#
-#         self.min_length = min_length
-#
-#     def __call__(self, value):
-#
-#         # super(MinLengthValidator, self).__call__(value)
-#         if len(value) < self.min_length:
-#             raise ValidationError(
-#                 'Length must be at least {0}; received value <{1}> of length {2}'.format(
-#                     self.min_length,
-#                     value,
-#                     len(value)
-#                 )
-#             )
-#
-# # class MaxLengthValidator(StringValidator):
-# class MaxLengthValidator(object):
-#
-#     def __init__(self, max_length):
-#
-#         self.max_length = max_length
-#
-#     def __call__(self, value):
-#
-#         # super(MaxLengthValidator, self).__call__(value)
-#         if len(value) > self.max_length:
-#             raise ValidationError(
-#                 'Length must be less than or equal to {0}; received value <{1}> of length {2}'.format(
-#                     self.max_length,
-#                     value,
-#                     len(value)
-#                 )
-#             )
-
-
 # Adapted from Django RegexValidator
 import re
-class RegexValidator(TypeValidator):
+class RegexValidator(object):
 
     def __init__(self, regex=None, flags=0):
 
@@ -85,7 +46,6 @@ class RegexValidator(TypeValidator):
 
     def __call__(self, value):
 
-        super(RegexValidator, self).__call__(value)
         if not self.regex.search(value):
             raise ValidationError(
                 'Value must match regex {0} and flags {1}; received value <{2}>'.format(
