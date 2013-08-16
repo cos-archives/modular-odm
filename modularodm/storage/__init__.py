b281835b9413f9c886c4844bbf440f7847339a20
Joshua Carp <jm.carp@gmail.com>
2013-08-16 10:13:06 -0400

    add logging to backends

--- modularodm/storage/__init__.py
+++ modularodm/storage/__init__.py
@@ -2,22 +2,112 @@
 
 """
 
+import time
 import random
 import string
+import logging
+from functools import wraps
+import itertools
+import operator
 
 from ..translators import DefaultTranslator
 from ..translators import JSONTranslator
 
 class KeyExistsException(Exception): pass
 
-# class StorageMeta(type):
-#
-#     def __init__(cls, name, bases, dct):
-#
-#         # Run super-metaclass __init__
-#         super(SOMeta, cls).__init__(name, bases, dct)
-#
-#
+class Logger(object):
+
+    def __init__(self):
+
+        self.listening = False
+        self.events = []
+
+    def listen(self):
+
+        if self.listening:
+            return False
+
+        self.listening = True
+        self.events = []
+        return True
+
+    def record_event(self, event):
+
+        if self.listening:
+            self.events.append(event)
+
+    def report(self):
+
+        out = {}
+
+        comparator = operator.attrgetter('func_name')
+        heard = sorted(self.events, key=comparator)
+        for key, group in itertools.groupby(heard, comparator):
+            group = list(group)
+            num_events = len(group)
+            total_time = sum([event.elapsed_time for event in group])
+            out[key] = (num_events, total_time)
+
+        return out
+
+    def clear(self):
+
+        self.listening = False
+        self.events = []
+
+class LogEvent(object):
+
+    def __init__(self, func_name, start_time, stop_time):
+
+        self.func_name = func_name
+        self.start_time = start_time
+        self.stop_time = stop_time
+        self.elapsed_time = stop_time - start_time
+
+    def __repr__(self):
+
+        return 'LogEvent("{func_name}", {start_time}, {stop_time})'.format(
+            **self.__dict__
+        )
+
+def logify(func):
+
+    @wraps(func)
+    def wrapped(this, *args, **kwargs):
+
+        if this.logger.listening:
+            start_time = time.time()
+
+        out = func(this, *args, **kwargs)
+
+        if this.logger.listening:
+            stop_time = time.time()
+            this.logger.record_event(LogEvent(func.__name__, start_time, stop_time))
+
+        return out
+
+    return wrapped
+
+class StorageMeta(type):
+
+    def __new__(mcs, name, bases, dct):
+
+        for key, value in dct.items():
+
+            if not hasattr(value, '__call__'):
+                continue
+            if isinstance(value, type):
+                continue
+            if key.startswith('_') or key.endswith('_'):
+                continue
+
+            dct[key] = logify(value)
+
+        # if not issubclass(mcs, Storage):
+        dct['logger'] = Logger()
+
+        # Run super-metaclass __new__
+        return super(StorageMeta, mcs).__new__(mcs, name, bases, dct)
 
 class Storage(object):
     """Abstract base class for storage objects. Subclasses (e.g. PickleStorage,
@@ -26,7 +116,7 @@ class Storage(object):
 
     """
 
-    Translator = DefaultTranslator
+    __metaclass__ = StorageMeta
     Translator = JSONTranslator
 
     def _ensure_index(self, key):
