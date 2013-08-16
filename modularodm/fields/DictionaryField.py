bbcbacc631a9479887d42ff53feef822be70c8e9
Joshua Carp <jm.carp@gmail.com>
2013-08-16 12:53:03 -0400

    translate_type -> data_type

--- modularodm/fields/DictionaryField.py
+++ modularodm/fields/DictionaryField.py
@@ -1,7 +1,7 @@
 from ..fields import Field
 
 class DictionaryField(Field):
-    translate_type = dict
+    data_type = dict
 
     def __init__(self, *args, **kwargs):
         super(self.__class__, self).__init__(*args, **kwargs)
