7b22d8bdb9eadac8d62dad8829ce9e79771dd52c
Joshua Carp <jm.carp@gmail.com>
2013-08-16 12:52:28 -0400

    add auto_now_add

--- modularodm/fields/DateTimeField.py
+++ modularodm/fields/DateTimeField.py
@@ -1,32 +1,27 @@
-# todo: override to_storage
-
 from . import Field
-from ..exceptions import ValidationError
 from ..validators import validate_datetime
 
 import datetime
-from dateutil import parser
 
 class DateTimeField(Field):
 
-    # Method will be called on SchemaObject __init__
-    default = datetime.datetime.utcnow
     validate = validate_datetime
-    translate_type = datetime.datetime
+    data_type = datetime.datetime
 
     def __init__(self, *args, **kwargs):
-        super(DateTimeField, self).__init__(*args, **kwargs)
-        self._auto_now = kwargs.pop('auto_now', False)
 
-    # todo: auto_now updates datetimes on save; should there be an option to update on modify?
+        super(DateTimeField, self).__init__(*args, **kwargs)
+
+        self._auto_now = kwargs.pop('auto_now', False)
+        self._auto_now_add = kwargs.pop('auto_now_add', False)
+        if self._auto_now and self._auto_now_add:
+            raise Exception("Can't use auto_now and auto_now_add on the same field.")
+
+        if self._auto_now or self._auto_now_add:
+            self._editable = False
+        if self._auto_now_add:
+            self._default = datetime.datetime.now
+
     def on_before_save(self, instance):
         if self._auto_now:
-            self.data[instance] = self.default()
-
-    # # todo: abstract this as optional Field.transform() or Field.preprocess() method
-    # def __set__(self, instance, value):
-    #     try:
-    #         self.do_validate(value)
-    #     except ValidationError:
-    #         value = parser.parse(value)
-    #     super(DateTimeField, self).__set__(instance, value)
+            self.data[instance] = datetime.datetime.now()
