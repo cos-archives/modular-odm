207cfc0db9af40041a3abfe18e094a4b327819bd
Lyndsy Simon <lyndsy@lyndsysimon.com>
2013-08-20 15:29:05 -0400

    Fleshes out basic query tests (failing)

--- modularodm/tests/queries/test_simple_queries.py
+++ modularodm/tests/queries/test_simple_queries.py
@@ -1,28 +1,78 @@
-from modularodm.tests import PickleStorageTestCase
+from modularodm import StoredObject
+from modularodm.storage.PickleStorage import PickleStorage
+from modularodm.fields.IntegerField import IntegerField
+from modularodm.query.query import RawQuery as Q
+
+import unittest
+import os
 
 
-class BasicQueryBase(object):
+class BasicQueryBase(unittest.TestCase):
+
+    make_storage = lambda x: PickleStorage('Test')
+
+    def setUp(self):
+        super(BasicQueryBase, self).setUp()
+        self.set_up_objects()
+
+    def tearDown(self):
+        super(BasicQueryBase, self).tearDown()
+        self.tear_down_objects()
+
+    def set_up_objects(self):
+        class Foo(StoredObject):
+            _id = IntegerField(primary=True)
+
+        Foo.set_storage(self.make_storage())
+        Foo._clear_caches()
+
+
+        self.Foo = Foo
+        self.foos = []
+
+        for idx in xrange(3):
+            foo = Foo(_id=idx)
+            foo.save()
+            self.foos.append(foo)
+
+    def tear_down_objects(self):
+        try:
+            os.remove('db_Test.pkl')
+        except OSError:
+            pass
+
+    @unittest.skip('Not sure yet if this should be .get() or .load()')
     def test_get_by_pk(self):
         """ Given a known primary key, ``.get(pk)``  should return the object.
         """
-        pass
+        self.assertEqual(
+            self.foos[0],
+            self.Foo.get(0)
+        )
 
     def test_find_all(self):
         """ If no query object is passed, ``.find()`` should return all objects.
         """
-        pass
+        self.assertEqual(
+            len(self.Foo.find()),
+            len(self.foos)
+        )
 
     def test_find_one(self):
         """ Given a query with exactly one result record, ``.find_one()`` should
         return that object.
         """
-        pass
+        self.assertEqual(
+            self.Foo.find_one(Q('_id', 'eq', '0')),
+            self.foos[0]
+        )
 
     def test_find_one_return_zero(self):
         """ Given a query with zero result records, ``.find_one()`` should raise
          an appropriate error.
         """
-        pass
+
+        self.Foo.find_one(Q('_id', 'eq', '-1'))
 
     def test_find_on_return_many(self):
         """ Given a query with >1 result record, ``.find_one()`` should raise
@@ -31,8 +81,6 @@ class BasicQueryBase(object):
         pass
 
 
-class BasicQueryPickleTestCase(BasicQueryBase, PickleStorageTestCase):
-    pass
 
 
 # TODO: MongoStorageTestCase not yet implemented
