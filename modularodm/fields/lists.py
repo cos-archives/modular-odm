72aed94440f8a62b0ef135dbb19fd401233964ed
Joshua Carp <jm.carp@gmail.com>
2013-08-21 09:17:23 -0400

    store base_class rather than field_instance

--- modularodm/fields/lists.py
+++ modularodm/fields/lists.py
@@ -5,21 +5,21 @@ class List(list):
 
         super(List, self).__init__()
 
-        self._field_instance = kwargs.get('field_instance', None)
+        self._base_class = kwargs.get('base_class', None)
 
         if value is None:
             return
 
         for item in value:
-            self.append(value)
+            self.append(item)
 
 class ForeignList(List):
 
     def _pk(self, value):
-        return self._field_instance._to_primary_key(value)
+        return self._base_class._to_primary_key(value)
 
     def _to_primary_keys(self):
-        return [self._pk(item) for item in super(ForeignList, self).__iter__()]
+        return list(super(ForeignList, self).__iter__())
 
     def __iter__(self):
         if self:
@@ -29,15 +29,13 @@ class ForeignList(List):
     def __getitem__(self, item):
         result = super(ForeignList, self).__getitem__(item)
         if isinstance(result, list):
-            return [self._field_instance.base_class.load(i) for i in result]
-        return self._field_instance.base_class.load(result)
+            return [self._base_class.load(i) for i in result]
+        return self._base_class.load(result)
 
     def __contains__(self, item):
-
-        base_class = self._field_instance.base_class
-        if isinstance(item, base_class):
+        if isinstance(item, self._base_class):
             return item._primary_key in self._to_primary_keys()
-        if isinstance(item, base_class._primary_type):
+        if isinstance(item, self._base_class._primary_type):
             return item in self._to_primary_keys()
         return False
 
