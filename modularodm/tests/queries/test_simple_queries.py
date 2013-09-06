cd60c55a60bdc070866e2e12311ebdc9a704000e
Lyndsy Simon <lyndsy@lyndsysimon.com>
2013-09-06 16:39:01 -0400

    Adds tests for .limit() and .offset()

--- modularodm/tests/queries/test_simple_queries.py
+++ modularodm/tests/queries/test_simple_queries.py
@@ -8,7 +8,7 @@ import pymongo
 import unittest
 
 
-class BasicQueryPickleTestCase(ModularOdmTestCase):
+class BasicQueryTestCase(ModularOdmTestCase):
 
     def define_test_objects(self):
         class Foo(StoredObject):
@@ -19,18 +19,17 @@ class BasicQueryPickleTestCase(ModularOdmTestCase):
     def set_up_test_objects(self):
         self.foos = []
 
-        for idx in xrange(3):
+        for idx in xrange(30):
             foo = self.Foo(_id=idx)
             foo.save()
             self.foos.append(foo)
 
-    @unittest.skip('Not sure yet if this should be .get() or .load()')
-    def test_get_by_pk(self):
+    def test_load_by_pk(self):
         """ Given a known primary key, ``.get(pk)``  should return the object.
         """
         self.assertEqual(
             self.foos[0],
-            self.Foo.get(0)
+            self.Foo.load(0)
         )
 
     def test_find_all(self):
@@ -63,3 +62,51 @@ class BasicQueryPickleTestCase(ModularOdmTestCase):
         """
         with self.assertRaises(exceptions.MultipleResultsFound):
             print self.Foo.find_one()
+
+    def test_limit(self):
+        """ For a query that returns > n results, `.limit(n)` should return the
+         first n.
+        """
+        self.assertEqual(
+            len(self.Foo.find().limit(10)),
+            10,
+        )
+
+    def test_offset(self):
+        """For a query that returns n results, ``.offset(m)`` should return
+        n - m results, skipping the first m that would otherwise have been
+        returned.
+        """
+        results = self.Foo.find().offset(25)
+        self.assertSetEqual(
+            set([x._id for x in results]),
+            {25, 26, 27, 28, 29}
+        )
+
+    def test_limit_offset(self):
+        results = self.Foo.find().limit(10).offset(10)
+        self.assertSetEqual(
+            set([x._id for x in results]),
+            set()
+        )
+
+    def test_offset_limit(self):
+        results = self.Foo.find().offset(10).limit(5)
+        self.assertSetEqual(
+            set([x._id for x in results]),
+            {10, 11, 12, 13, 14}
+        )
+
+    def test_offset_limit_offset(self):
+        results = self.Foo.find().offset(10).limit(5).offset(1)
+        self.assertSetEqual(
+            set([x._id for x in results]),
+            {11, 12, 13, 14}
+        )
+
+    def test_limit_offset_limit(self):
+        results = self.Foo.find().limit(10).offset(5).limit(2)
+        self.assertSetEqual(
+            set([x._id for x in results]),
+            {5, 6}
+        )
