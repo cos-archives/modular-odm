f4779669c9f28ab4fae19b6442008ab56faa03f8
Acshi Haggenmiller <acshikh@gmail.com>
2016-08-30 16:55:45 -0400

    added additional test for invalid user url, and corrected regex to work with it. changed tests to pull from json file identical to the one used on the OSF for testing url validation

--- modularodm/validators/__init__.py
+++ modularodm/validators/__init__.py
@@ -85,7 +85,7 @@ class RegexValidator(Validator):
 class URLValidator(RegexValidator):
     regex = re.compile(
         u'^(?:(?:https?|ftp)://)?'  # http:// or https://
-        u'(?:\S+(?::\S*)?@)?'  # user:passauthentication
+        u'(?:[^\/\s]+(?::[^\/\s]*)?@)?'  # user:passauthentication
         u'(?:(?:(?:[\\u00a1-\uffffA-Z0-9][\\u00a1-\uffffA-Z0-9-]{0,61}[\\u00a1-\uffffA-Z0-9]?\.){1,2}(?:[\\u00a1-\uffffA-Z0-9]{2,}))|' # domain
         u'localhost|'  # localhost
         u'(?:(?:(?:25[0-5]|2[0-4][0-9]|[1]?[0-9]?[0-9])\.){3}(?:25[0-5]|2[0-4][0-9]|[1]?[0-9]?[0-9]))|'  # or ipv4
--- tests/validators/test_url_validation.py
+++ tests/validators/test_url_validation.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+import os.path
+import json
+
 from modularodm import StoredObject
 from modularodm.exceptions import ValidationError
 from modularodm.fields import StringField, IntegerField
@@ -10,32 +13,10 @@ from tests.base import ModularOdmTestCase
 class UrlValueValidatorTestCase(ModularOdmTestCase):
 
     def test_url(self):
-
-        testsPositive = [u'http://Bücher.de', u'http://localhost:5000/meetings', u'http://foo.com/blah_blah',
-                         u'http://foo.com/blah_blah/', u'http://foo.com/blah_blah_(wikipedia)',
-                         u'http://userid:password@example.com:8080', u'http://userid:password@example.com:8080/',
-                         u'http://userid@example.com', u'http://userid@example.com/', u'http://userid@example.com:8080',
-                         u'http://userid@example.com:8080/', u'http://userid:password@example.com',
-                         u'http://userid:password@example.com/', u'http://142.42.1.1', u'http://142.42.1.1/',
-                         u'http://142.42.1.1:8080/',
-                         u'http://➡.ws/䨹', u'http://⌘.ws', u'http://⌘.ws/', u'http://foo.com/blah_(wikipedia)#cite-1',
-                         u'http://foo.com/blah_(wikipedia)_blah#cite-1', u'http://foo.com/unicode_(✪)_in_parens',
-                         u'http://foo.com/(something)?after=parens', u'http://☺.damowmow.com/',
-                         u'http://code.google.com/events/#&product=browser', u'http://j.mp', u'ftp://foo.bar/baz',
-                         u'http://foo.bar/?q=Test%20URL-encoded%20stuff', u'http://مثال.إختبار', u'http://例子.测试',
-                         u'http://उदाहरण.परीक्षा', u'http://-.~_!$&\'()*+,;=:%40:80%2f::::::@example.com',
-                         u'http://1337.net', u'definitelyawebsite.com?real=yes&page=definitely',
-                         u'http://a.b-c.de',
-                         u'http://10.1.1.0', u'http://10.1.1.255', u'http://224.1.1.1', u'foo.com', ]
-
-        testsNegative = [u'http://', u'http://.', u'http://..', u'http://../',
-                         u'http://?', u'http://??', u'http://??/', u'http://#', u'http://##', u'http://##/',
-                         u'http://foo.bar?q=Spaces should be encoded', u'//', u'//a', u'///a', u'///', u'http:///a',
-                         u'rdar://1234', u'h://test', u'http:// shouldfail.com', u':// should fail',
-                         u'http://foo.bar/foo(bar)baz quux', u'ftps://foo.bar/', u'http://-error-.invalid/',
-                         u'http://1.1.1.1.1', u'http://-a.b.co',
-                         u'http://3628126748', u'http://.www.foo.bar/', u'http://www.foo.bar./',
-                         u'http://.www.foo.bar./']
+        basepath = os.path.dirname(__file__)
+        url_data_path = os.path.join(basepath, 'urlValidatorTest.json')
+        with open(url_data_path) as url_test_data:
+            data = json.load(url_test_data)
 
         class Foo(StoredObject):
             _id = IntegerField()
@@ -47,11 +28,11 @@ class UrlValueValidatorTestCase(ModularOdmTestCase):
         Foo.set_storage(self.make_storage())
         test_object = Foo()
 
-        for urlTrue in testsPositive:
+        for urlTrue in data['testsPositive']:
             test_object.url_field = urlTrue
             test_object.save()
 
-        for urlFalse in testsNegative:
+        for urlFalse in data['testsNegative']:
             test_object.url_field = urlFalse
             with self.assertRaises(ValidationError):
                 test_object.save()
